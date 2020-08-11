eb57c6539d969b04e5869ebca93abb80d01f5ef7
Helena Tran <htran30@huskers.unl.edu>
2020-08-11 02:30:18 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/** 
+ * Author: Helena Tran
+ * Date: 2020/08/10
+ * 
+ * A simple hello world program in C
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     printf("Hello World!\n");
+     
+     return 0;
+ }
+ 
+ 
+ 
